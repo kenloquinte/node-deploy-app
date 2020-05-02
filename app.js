548754ac952b6c5398918b7620fd916668b5262f
kenloquinte <kennethloquinte@gmail.com>
2020-05-02 15:35:30 +0800

    Update code for deployment

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const MongoStore = require('connect-mongo')(session);
 const moment = require('moment');
+const { envPort, sessionKey } = require('./config');
+
 
 // Routes imports
 const authRouter = require('./routes/auth');
@@ -17,7 +19,7 @@ const postsRouter = require('./routes/postRoutes');
 
 // Creates the express application
 const app = express();
-const port = 9090;
+const port = envPort || 9090;
 
 // Listening to the port provided
 app.listen(port, () => {
@@ -60,7 +62,7 @@ app.use(express.static('public'));
 // Insert server configuration after this comment
 // Sessions
 app.use(session({
-  secret: 'somegibberishsecret',
+  secret: sessionKey,
   store: new MongoStore({ mongooseConnection: mongoose.connection }),
   resave: false,
   saveUninitialized: true,
